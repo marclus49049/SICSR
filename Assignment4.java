27f9946cab4b8230fb31e31b100a65c56db2cf3d
Marclus Lopes <marcluslopes@gmail.com>
2018-12-17 20:23:51 +0530

    Assignment 4 Completed

--- Assignment4.java
+++ Assignment4.java
@@ -36,12 +36,58 @@ public class Assignment4 extends AppCompatActivity implements View.OnClickListen
         submit.setOnClickListener(this);
     }
 
+    int sum(int num){
+        int sum=0, mod=0;
+        while(num!=0){
+            mod = num % 10;
+            sum = sum + mod;
+            num = num / 10;
+        }
+        return(sum);
+    }
+
+    int reverse(int num){
+        int reverse = 0, mod = 0;
+        while(num!=0){
+            mod = num%10;
+            reverse = (reverse*10) + mod;
+            num = num / 10;
+        }
+        return(reverse);
+    }
+
+    boolean Prime(int num){
+        boolean isPrime = true;
+        for(int i=2; i<num; i++){
+            if(num%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        return(isPrime);
+    }
+
     int num = 0;
     @Override
     public void onClick(View v) {
         int op = operation.getSelectedItemPosition();
         if(number.getText().length()>0){
             num = Integer.parseInt(number.getText().toString());
+        }
+        switch (op){
+            case 0:
+                Toast.makeText(this, "Sum of "+num+" is "+sum(num), Toast.LENGTH_LONG).show();
+                break;
+            case 1:
+                Toast.makeText(this, "Reverse of "+num+" is "+reverse(num), Toast.LENGTH_LONG).show();
+                break;
+            case 2:
+                if(Prime(num)){
+                    Toast.makeText(this, num+" is a Prime Number", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, num+" is not a Prime Number", Toast.LENGTH_LONG).show();
+                }
+                break;
         }
     }
 }
